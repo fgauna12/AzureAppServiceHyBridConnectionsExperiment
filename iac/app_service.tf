7a47fcfb33d7cf5545d36344edf609ff3507faff
Facundo Gauna <facundo.gauna@newsignature.com>
2020-04-04 00:23:25 -0400

    Fixing location

--- iac/app_service.tf
+++ iac/app_service.tf
@@ -1,6 +1,6 @@
 resource "azurerm_app_service" "app_service" {
   name                = local.app_service_name
-  location            = azurerm_resource_group.resource_group.LOCATION
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   app_service_plan_id = azurerm_app_service_plan.app_plan.id
 
@@ -24,7 +24,7 @@ resource "azurerm_app_service" "app_service" {
 
 resource "azurerm_app_service_plan" "app_plan" {
   name                = local.app_service_plan_name
-  location            = azurerm_resource_group.resource_group.LOCATION
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 
   sku {
